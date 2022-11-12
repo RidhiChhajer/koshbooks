01968e1d56d1542157cb5069ac75fa67c02e65b7
Rahul Gunaseelan <rahulgunaseelan@gmail.com>
2022-11-12 11:12:04 +0530

    Better

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Cart from './components/cart/cartReact'
 import Ccu from './components/ccu/ccuReact'
 import Profile from './components/profile/profileReact'
 import Wishlist from './components/wishlist/wishlistReact'
-import Loading from './components/Loading';
 // import Cookies from "js-cookie";
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import axios from 'axios';
@@ -15,33 +14,27 @@ import { useEffect, useState } from 'react';
 
 const App = () => {
 
-  const [ load, setLoad ] = useState();
   const [ user, setUser ] = useState();
-
-  if (!load) {
-    const fetchUser = async () => {
-      const { data } = await axios.get(API + `user`, {
-        withCredentials: true,
-      });
-      console.log("User");
-      console.log(data);
-      setUser(data);
-    };
-    fetchUser();
-  }
-
+  const fetchUser = async () => {
+    const { data } = await axios.get(API + `user`, {
+      withCredentials: true,
+    });
+    console.log("User");
+    console.log(data);
+    setUser(data);
+  };
   useEffect(() => {
-    setLoad(true)
-  }, [])
+    fetchUser();
+  }, []);
 
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Landing} />
         <Route exact path='/explore' component={Explore} />
-        <Route exact path='/auth' component={load ? () => (user !== undefined ? <Redirect to='/explore' /> : <Login />) : <Loading />} />
+        <Route exact path='/auth' component={() => user === undefined ? <Login /> : <Redirect to='/explore' />} />
         <Route exact path='/books/:id' component={BookDetails} />
-        <Route exact path='/profile' component={load ? () => (user !== undefined ? <Profile /> : <Redirect to="/auth" />) : <Loading />} />
+        <Route exact path='/profile' component={() => user === undefined ? <Redirect to='/auth' /> : <Profile />} />
         <Route exact path='/cart' component={Cart} />
         <Route exact path='/ccu' component={Ccu} />
         <Route exact path='/wishlist' component={Wishlist} />
